a880bb228d03119f50cbc32c1fb104c27f752b76
Ilya Bagrov <ilya.b.only@gmail.com>
2024-08-28 23:25:43 +0300

    Implement a path operation func for creating notes

--- notes/api/v1/router.py
+++ notes/api/v1/router.py
@@ -1,5 +1,9 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from database.db import get_db_session
+from database.models import NoteModel
+from validators import validate_note
 from schemas import CreateNote, Note
 
 
@@ -7,11 +11,15 @@ router = APIRouter(prefix='/api/v1', tags=['Notes'])
 
 @router.get('/notes', response_model=list[Note])
 async def get_notes_list():
-    notes = get_notes()
     return notes
 
 @router.post('/notes', status_code=status.HTTP_201_CREATED)
-async def create_new_note(raw_note: CreateNote):
-    note = validate_note(raw_note)
-    create_note(note)
+async def create_new_note(
+    raw_note: CreateNote,
+    db_session: AsyncSession = Depends(get_db_session)
+):
+    validated_raw_note = await validate_note(raw_note)
+    note = NoteModel(**validated_raw_note.model_dump())
+    db_session.add(note)
+    await db_session.commit()
     return {'status': 'success', 'message': 'Note created successfully'}
