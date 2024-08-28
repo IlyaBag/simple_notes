6dc3090be8c2dfd8e4c7bcc00540536467ace7d4
Ilya Bagrov <ilya.b.only@gmail.com>
2024-08-28 19:53:39 +0300

    Add ORM model Note

--- notes/database/models.py
+++ notes/database/models.py
@@ -1,5 +1,20 @@
-from sqlalchemy.orm import DeclarativeBase
+from datetime import datetime
+from typing import Optional
+
+from sqlalchemy import TIMESTAMP, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
     pass
+
+
+class NoteModel(Base):
+    __tablename__ = 'notes'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(100))
+    content: Mapped[Optional[str]]
+    created_at: Mapped[TIMESTAMP] = mapped_column(TIMESTAMP(timezone=True),
+                                                  nullable=False,
+                                                  default=datetime.now)
