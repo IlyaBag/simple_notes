92a34e1502252e2e974bfa00f2c2fa8f681214f1
Ilya Bagrov <ilya.b.only@gmail.com>
2024-08-28 19:45:58 +0300

    Add ORM with async access to database

---
+++ notes/database/db.py
@@ -0,0 +1,15 @@
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+
+from config import DB_ECHO, DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
+
+
+engine = create_async_engine(
+    f'postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}',
+    echo=DB_ECHO,
+)
+
+async_session_factory = async_sessionmaker(
+    engine,
+    autoflush=False,
+    expire_on_commit=False,
+)
--- notes/main.py
+++ notes/main.py
@@ -1,8 +1,18 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from api.v1.router import router as notes_router
+from database.db import engine
+from database.models import Base
 
 
-app = FastAPI(title='Simple Notes')
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+app = FastAPI(lifespan=lifespan, title='Simple Notes')
 
 app.include_router(notes_router)
