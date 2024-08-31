4b0a8dd1d721c07629eff86e213fc4c41b2bef69
Ilya Bagrov <ilya.b.only@gmail.com>
2024-08-31 20:11:51 +0300

    Remove auth router

--- notes/api_v1/__init__.py
+++ notes/api_v1/__init__.py
@@ -1,10 +1,8 @@
 from fastapi import APIRouter
 
-from .auth import router as auth_router
 from .notes_api import router as notes_router
 
 
 router = APIRouter(prefix='/api/v1')
 
 router.include_router(notes_router)
-router.include_router(auth_router)
