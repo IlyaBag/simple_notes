0361d69eebbc04585e7d1dfa71862ae6cb22abbd
Ilya Bagrov <ilya.b.only@gmail.com>
2024-08-31 20:09:34 +0300

    Add basic authentication function

--- notes/api_v1/auth.py
+++ notes/api_v1/auth.py
@@ -1,4 +1,34 @@
-from fastapi import APIRouter
+import secrets
+from typing import Annotated
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 
-router = APIRouter(prefix='/auth', tags=['Authentication'])
+security = HTTPBasic()
+
+USERS = {
+    'admin': 'admin',
+    'usser': 'qwerty',
+}
+
+def get_auth_user_username(
+    credentials: Annotated[HTTPBasicCredentials, Depends(security)]
+):
+    unauthed_exc = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail='Incorrect username or password',
+        headers={'WWW-Authenticate': 'Basic'}
+    )
+
+    correct_password = USERS.get(credentials.username)
+    if not correct_password:
+        raise unauthed_exc
+
+    if not secrets.compare_digest(
+        credentials.password.encode('utf-8'),
+        correct_password.encode('utf-8')
+    ):
+        raise unauthed_exc
+
+    return credentials.username
