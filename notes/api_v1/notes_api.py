93ce5a780c18e6f5649b0c95b730d19f58a21975
Ilya Bagrov <ilya.b.only@gmail.com>
2024-08-31 20:16:48 +0300

    Add auth dependency to note's endpoints
    Note saves in database with username of current user.
    User can get notes only after logging in.
    User can only get their own notes.

--- notes/api_v1/notes_api.py
+++ notes/api_v1/notes_api.py
@@ -4,26 +4,33 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from database.db import get_db_session
 from database.models import NoteModel
+from api_v1.auth import get_auth_user_username
 from validators import validate_note
 from schemas import CreateNote, Note
 
 
 router = APIRouter(prefix='/notes', tags=['Notes'])
 
+
 @router.get('/', response_model=list[Note])
-async def get_notes_list(db_session: AsyncSession = Depends(get_db_session)):
-    stmt = select(NoteModel).order_by(NoteModel.created_at)
+async def get_notes_list(
+    db_session: AsyncSession = Depends(get_db_session),
+    username: str = Depends(get_auth_user_username)
+):
+    stmt = select(NoteModel).where(NoteModel.username==username).order_by(NoteModel.created_at)
     result: Result = await db_session.execute(stmt)
     notes = result.scalars().all()
     return notes
 
+
 @router.post('/', status_code=status.HTTP_201_CREATED)
 async def create_new_note(
     raw_note: CreateNote,
-    db_session: AsyncSession = Depends(get_db_session)
+    db_session: AsyncSession = Depends(get_db_session),
+    username: str = Depends(get_auth_user_username)
 ):
     validated_raw_note = await validate_note(raw_note)
-    note = NoteModel(**validated_raw_note.model_dump())
+    note = NoteModel(username=username, **validated_raw_note.model_dump())
     db_session.add(note)
     await db_session.commit()
     return {'status': 'success', 'message': 'Note created successfully'}
